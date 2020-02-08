5bf4c1bc6a7f7628a0a41a0850054ec877fb9cb0
Yidan Ma <madt@bc.edu>
2020-02-08 17:56:25 -0500

    5.3 - Working with Multiple View Controllers + Segues

--- ToDo List/ToDoListViewController.swift
+++ ToDo List/ToDoListViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ToDoListViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
-    var toDoArray = ["Happy", "Sad", "Smile"]
+    var toDoArray = ["Learn Swift", "Build Apps", "Change the World", "Take a Vacation"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
