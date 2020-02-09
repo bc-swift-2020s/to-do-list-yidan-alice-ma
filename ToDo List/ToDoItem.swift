3f49031e07933ef5768405b5b440edeea30b5bb4
Yidan Ma <madt@bc.edu>
2020-02-08 23:00:10 -0500

    5.6 - Structs

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by mayidan on 2/8/20.
+//  Copyright © 2020 mayidan. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
