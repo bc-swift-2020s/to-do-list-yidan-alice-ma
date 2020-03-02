2638b445885aac174b1122daf52499c37b65b08f
Yidan Ma <madt@bc.edu>
2020-03-02 15:15:01 -0500

    5.10 - Local Notifications

--- ToDo List/AppDelegate.swift
+++ ToDo List/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        UNUserNotificationCenter.current().delegate = self
+        
         return true
     }
 
@@ -33,5 +36,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let id = notification.request.identifier
+        print("Received in-app notification with ID = \(id)")
+        UNUserNotificationCenter.current().removeAllDeliveredNotifications()
+        completionHandler([.alert, .sound])
+    }
 }
 
